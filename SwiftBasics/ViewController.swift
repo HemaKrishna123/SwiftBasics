90a420d68deac4d7dfe0876a3a903e1a628d55b6
Hema <hemakrishna1435@gmail.com>
2023-03-12 16:03:44 +0530

    Initial Commit

---
+++ SwiftBasics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBasics
+//
+//  Created by Apple on 12/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
